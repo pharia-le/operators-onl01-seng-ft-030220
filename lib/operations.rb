58b1aa1e4991557bb695d997ea02f902f3cf1616
Pharia Le <pharia.le@gmail.com>
2020-03-03 04:41:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if (speed < 40 || speed > 60)
+  if speed < 40 || speed > 60
     return true
   else
     return false
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-  (speed < 40 || speed > 60) ? return true : return false
+  speed < 40 || speed > 60 ? return true : return false
 end
 
 
